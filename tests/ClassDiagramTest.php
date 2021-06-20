10cd1abfd4b6ad43b88d12a9aca066a853496343
opmvpc <thibsix@outlook.be>
2021-06-20 13:26:21 +0200

    first project classes

---
+++ tests/ClassDiagramTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Opmvpc\ClassDiagram\Tests;
+
+use Opmvpc\ClassDiagram\ClassDiagram;
+use PHPUnit\Framework\TestCase;
+
+class ClassDiagramTest extends TestCase
+{
+
+}
---
+++ tests/resources/SimpleClass/SimpleClass.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Opmvpc\ClassDiagram\Tests\resources\SimpleClass;
+
+
+class SimpleClass
+{
+
+}
